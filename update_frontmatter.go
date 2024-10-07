6ff6d1367fd05b28a066388c76b96ff2601b48f2
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-10-07 15:12:00 +0530

    [UI] applying suggested changes
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- update_frontmatter.go
+++ update_frontmatter.go
@@ -33,49 +33,59 @@ func main() {
 		return
 	}
 
-	// Iterate over the JSON data
+	// Group entries by documentation URL
+	groupedEntries := make(map[string][]PricingData)
 	for _, entry := range pricingData {
 		if strings.HasPrefix(entry.Documentation, "https://docs.layer5.io/") {
-			fmt.Println("\n--- Processing documentation URL ---")
-			fmt.Println("Documentation URL:", entry.Documentation)
-
-			// Remove the base URL and split by "/"
-			urlParts := strings.Split(strings.TrimPrefix(entry.Documentation, "https://docs.layer5.io/"), "/")
-
-			// Initialize folder path to "content/en/"
-			folderPath := "content/en"
-			fmt.Println("Starting folder path:", folderPath)
-
-			// Extract hash if present
-			urlHash := ""
-			for i, part := range urlParts {
-				if strings.HasPrefix(part, "#") {
-					urlHash = strings.TrimPrefix(part, "#")
-					urlParts = urlParts[:i]
-					break
-				}
-			}
-
-			// Build the path step by step
-			for _, part := range urlParts {
-				if part == "" {
-					continue
-				}
-				folderPath = filepath.Join(folderPath, part)
-				fmt.Println("Updated folder path:", folderPath)
-			}
-
-			// Target the _index.md file inside the last folder
-			filePath := filepath.Join(folderPath, "_index.md")
-			fmt.Println("Targeting _index.md file:", filePath)
-
-			// Process the file
-			processFile(filePath, entry, urlHash)
+			groupedEntries[entry.Documentation] = append(groupedEntries[entry.Documentation], entry)
 		}
+	}
+
+	// Process each group
+	for docURL, entries := range groupedEntries {
+		fmt.Println("\n--- Processing documentation URL ---")
+		fmt.Println("Documentation URL:", docURL)
+
+		// Extract path and hash
+		urlParts, urlHash := extractHash(docURL)
+
+		// Build the folder path
+		folderPath := buildPath("content/en", urlParts)
+		fmt.Println("Final folder path:", folderPath)
+
+		// Target the _index.md file inside the last folder
+		filePath := filepath.Join(folderPath, "_index.md")
+		fmt.Println("Targeting _index.md file:", filePath)
+
+		// Process the file
+		processFileWithMultipleEntries(filePath, entries, urlHash)
 	}
 }
 
-func processFile(filePath string, entry PricingData, urlHash string) {
+func extractHash(url string) ([]string, string) {
+	urlParts := strings.Split(strings.TrimPrefix(url, "https://docs.layer5.io/"), "/")
+	var urlHash string
+	for i, part := range urlParts {
+		if strings.HasPrefix(part, "#") {
+			urlHash = strings.TrimPrefix(part, "#")
+			urlParts = urlParts[:i]
+			break
+		}
+	}
+	return urlParts, urlHash
+}
+
+func buildPath(basePath string, parts []string) string {
+	for _, part := range parts {
+		if part == "" {
+			continue
+		}
+		basePath = filepath.Join(basePath, part)
+	}
+	return basePath
+}
+
+func processFileWithMultipleEntries(filePath string, entries []PricingData, urlHash string) {
 	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -87,55 +97,83 @@ func processFile(filePath string, entry PricingData, urlHash string) {
 		}
 	}
 
-	shortcode := fmt.Sprintf(`{{< feature-support feature_name="%s" >}}`, entry.EntireRow.Feature)
 	contentStr := string(content)
-	var newContent string
+	tierFeatures := make(map[string][]string)
 
-	if urlHash == "" {
-		// No hash: add shortcode after the front matter
-		frontMatterEnd := strings.Index(contentStr, "---")
-		if frontMatterEnd != -1 {
-			frontMatterEnd = strings.Index(contentStr[frontMatterEnd+3:], "---")
-			if frontMatterEnd != -1 {
-				frontMatterEnd += 6 // Adjust for the second "---"
-				newContent = contentStr[:frontMatterEnd] + "\n" + shortcode + "\n" + contentStr[frontMatterEnd:]
-				fmt.Printf("Added shortcode after front matter in %s\n", filePath)
-			} else {
-				// If second "---" not found, add to the end
-				newContent = contentStr + "\n\n" + shortcode
-				fmt.Printf("Front matter end not found, added shortcode at the end of %s\n", filePath)
-			}
-		} else {
-			// If no front matter found, add to the beginning
-			newContent = shortcode + "\n\n" + contentStr
-			fmt.Printf("No front matter found, added shortcode at the beginning of %s\n", filePath)
-		}
-	} else {
-		// Hash present: search for matching heading and add shortcode below
-		hashPattern := strings.ReplaceAll(urlHash, "-", "[-\\s]")
-		hashRegex := regexp.MustCompile(`(?im)^#+\s*` + hashPattern)
-
-		hashMatch := hashRegex.FindStringIndex(contentStr)
-		if hashMatch != nil {
-			endOfLine := strings.Index(contentStr[hashMatch[1]:], "\n")
-			if endOfLine == -1 {
-				endOfLine = len(contentStr)
-			} else {
-				endOfLine += hashMatch[1]
-			}
-
-			newContent = contentStr[:endOfLine] + "\n\n" + shortcode + "\n" + contentStr[endOfLine:]
-			fmt.Printf("Added shortcode below matching heading for %s\n", filePath)
-		} else {
-			fmt.Printf("Hash '%s' not found in file %s, adding shortcode at the end\n", urlHash, filePath)
-			newContent = contentStr + "\n\n" + shortcode
-		}
+	// Collect features for each tier
+	for _, entry := range entries {
+		tier := strings.ToLower(entry.EntireRow.SubscriptionTier)
+		tierFeatures[tier] = append(tierFeatures[tier], entry.EntireRow.Feature)
 	}
 
-	err = ioutil.WriteFile(filePath, []byte(newContent), 0644)
+	// Process free tier
+	if freeFeatures, exists := tierFeatures["free"]; exists && len(freeFeatures) > 0 {
+		consolidatedFeature := strings.Join(freeFeatures, " ")
+		shortcode := fmt.Sprintf(`{{< feature-support feature_name="%s" >}}`, consolidatedFeature)
+		contentStr = insertConsolidatedShortcode(contentStr, shortcode, filePath)
+	}
+
+	// Process non-free tiers
+	for tier, features := range tierFeatures {
+		if tier == "free" {
+			continue
+		}
+		var tierShortcodes []string
+		for _, feature := range features {
+			shortcode := fmt.Sprintf(`{{< feature-support feature_name="%s" >}}`, feature)
+			tierShortcodes = append(tierShortcodes, shortcode)
+		}
+		contentStr = insertShortcodesAfterHeadings(contentStr, tierShortcodes, urlHash, filePath)
+	}
+
+	err = ioutil.WriteFile(filePath, []byte(contentStr), 0644)
 	if err != nil {
 		fmt.Printf("Error writing to file %s: %v\n", filePath, err)
 	} else {
-		fmt.Printf("Updated file with shortcode for %s\n", filePath)
+		fmt.Printf("Updated file with consolidated shortcodes for %s\n", filePath)
 	}
+}
+
+func insertConsolidatedShortcode(contentStr, shortcode, filePath string) string {
+	// Remove any existing feature-support shortcodes
+	re := regexp.MustCompile(`(?s){{<\s*feature-support.*?>}}`)
+	contentStr = re.ReplaceAllString(contentStr, "")
+
+	// Insert the consolidated shortcode after front matter
+	frontMatterEnd := strings.Index(contentStr, "---")
+	if frontMatterEnd != -1 {
+		frontMatterEnd = strings.Index(contentStr[frontMatterEnd+3:], "---")
+		if frontMatterEnd != -1 {
+			frontMatterEnd += 6 // Adjust for the second "---"
+			fmt.Printf("Added consolidated free shortcode after front matter in %s\n", filePath)
+			return contentStr[:frontMatterEnd] + "\n" + shortcode + "\n" + contentStr[frontMatterEnd:]
+		}
+	}
+	// If no front matter or second "---" found, insert at the beginning
+	fmt.Printf("Front matter end not found, added consolidated free shortcode at the beginning of %s\n", filePath)
+	return shortcode + "\n\n" + contentStr
+}
+
+func insertShortcodesAfterHeadings(contentStr string, shortcodes []string, urlHash string, filePath string) string {
+	hashPattern := strings.ReplaceAll(urlHash, "-", "[-\\s]")
+	hashRegex := regexp.MustCompile(`(?im)^#+\s*` + hashPattern)
+
+	hashMatch := hashRegex.FindStringIndex(contentStr)
+	if hashMatch != nil {
+		endOfLine := strings.Index(contentStr[hashMatch[1]:], "\n")
+		if endOfLine == -1 {
+			endOfLine = len(contentStr)
+		} else {
+			endOfLine += hashMatch[1]
+		}
+		shortcodesStr := strings.Join(shortcodes, "\n")
+		fmt.Printf("Added non-free shortcodes below matching heading for %s\n", filePath)
+		contentStr = contentStr[:endOfLine] + "\n\n" + shortcodesStr + "\n" + contentStr[endOfLine:]
+	} else {
+		// If hash not found, append at the end
+		fmt.Printf("Hash '%s' not found in file %s, adding non-free shortcodes at the end\n", urlHash, filePath)
+		shortcodesStr := strings.Join(shortcodes, "\n")
+		contentStr += "\n\n" + shortcodesStr
+	}
+	return contentStr
 }
