02e135892c255aa79f5720aebba68864427e9dd7
akshatsinghai6682-sketch <akshatsinghai6682-sketch@users.noreply.github.com>
2026-07-14 10:40:37 +0000

    Deploy preview for PR 1144 🛫

--- pr-preview/pr-1144/js/click-to-copy.js
+++ pr-preview/pr-1144/js/click-to-copy.js
@@ -1,7 +1,3 @@
-// Sistent Icon definitions exactly as provided by the design system
-const SISTENT_COPY_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#3C494F"><path d="M15.5 3H5.5C4.4 3 3.5 3.9 3.5 5V17H5.5V5H15.5V3ZM18.5 7H9.5C8.4 7 7.5 7.9 7.5 9V21C7.5 22.1 8.4 23 9.5 23H18.5C19.6 23 20.5 22.1 20.5 21V9C20.5 7.9 19.6 7 18.5 7ZM18.5 21H9.5V9H18.5V21Z"/></svg>`;
-const SISTENT_CHECK_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" /></svg>`;
-
 const codeListings = document.querySelectorAll('.highlight > pre');
 for (let index = 0; index < codeListings.length; index++) {
   const codeSample = codeListings[index].querySelector('code');
@@ -14,41 +10,29 @@ for (let index = 0; index < codeListings.length; index++) {
     'data-bs-placement': 'top',
     'data-bs-container': 'body',
   };
-
   Object.keys(buttonAttributes).forEach((key) => {
     copyButton.setAttribute(key, buttonAttributes[key]);
   });
-
-  // Removed FontAwesome classes ('fas', 'fa-copy') to keep only button styles
   copyButton.classList.add(
+    'fas',
+    'fa-copy',
     'btn',
     'btn-sm',
     'td-click-to-copy'
   );
-  
-  // Set initial Sistent Copy Icon
-  copyButton.innerHTML = SISTENT_COPY_ICON;
-  
   const tooltip = new bootstrap.Tooltip(copyButton);
-
   let revertTimeout;
-
   copyButton.onclick = () => {
   copyCode(codeSample)
     .then(() => {
       copyButton.setAttribute('data-bs-original-title', 'Copied!');
       tooltip.show();
-      
-      // Replaced FontAwesome toggle with Sistent Check Icon
-      copyButton.innerHTML = SISTENT_CHECK_ICON;
-      copyButton.classList.add('td-click-to-copy--copied');
-      
+      copyButton.classList.remove('fa-copy');
+      copyButton.classList.add('fa-check', 'td-click-to-copy--copied');
       clearTimeout(revertTimeout);
       revertTimeout = setTimeout(() => {
-        // Replaced FontAwesome toggle with Sistent Copy Icon
-        copyButton.innerHTML = SISTENT_COPY_ICON;
-        copyButton.classList.remove('td-click-to-copy--copied');
-        
+        copyButton.classList.remove('fa-check', 'td-click-to-copy--copied');
+        copyButton.classList.add('fa-copy');
         copyButton.setAttribute('data-bs-original-title', 'Copy to clipboard');
         tooltip.hide();
       }, 2500);
@@ -75,7 +59,6 @@ const copyCode = (codeSample) => {
     "code[data-lang='console'], code.language-console"
   );
   let text;
-
   if (isConsoleBlock) {
     const clone = codeSample.cloneNode(true);
     pruneUnselectableElements(codeSample, clone);
@@ -91,7 +74,6 @@ const copyCode = (codeSample) => {
 const pruneUnselectableElements = (sourceNode, cloneNode) => {
   const sourceChildren = sourceNode.children;
   const cloneChildren = cloneNode.children;
-
   for (let i = sourceChildren.length - 1; i >= 0; i--) {
     const sourceChild = sourceChildren[i];
     const cloneChild = cloneChildren[i];
@@ -102,7 +84,6 @@ const pruneUnselectableElements = (sourceNode, cloneNode) => {
       cloneChild.remove();
       continue;
     }
-
     pruneUnselectableElements(sourceChild, cloneChild);
   }
 };
