401f3c269067bb6ba15f04cfbc915d2d6b74c6f5
pontusringblom <pontusringblom@users.noreply.github.com>
2026-09-06 04:37:42 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-1232/js/kanvas-architectural-transition.js
@@ -0,0 +1,23 @@
+(function () {
+  "use strict";
+
+  function initTransition(container) {
+    var els = [
+      ".kanvas-ingress-gateway",
+      ".kanvas-kubernetes",
+      ".kanvas-pod",
+      ".kanvas-prometheus",
+      ".kanvas-supporting-arrows",
+    ].map(function (s) { return container.querySelector(s); }).filter(Boolean);
+
+    var active = false;
+    new IntersectionObserver(function (entries) {
+      var hit = entries[0].isIntersecting;
+      if (hit === active) return;
+      active = hit;
+      els.forEach(function (el) { el.classList.toggle("in-view", hit); });
+    }, { threshold: 0.7 }).observe(container);
+  }
+
+  window.initKanvasTransition = initTransition;
+})();
